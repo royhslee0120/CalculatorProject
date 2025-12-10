ac1c6b17dc826c16659d7bdec65503e6cae96046
royhsle0120 <royhslee0120@gmail.com>
2025-12-10 09:39:44 +0900

    양의 정수(0 포함)를 입력받기

--- src/calculator/App.java
+++ src/calculator/App.java
@@ -1,8 +1,18 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 양의 정수(0을 포함)를 입력하세요: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("두 번째 양의 정수(0을 포함)를 입력하세요: ");
+        int num2 = sc.nextInt();
 
     }
 }
