2ec55e05f1bdf074e88f0ae1fc5e9ded7b34feda
royhsle0120 <royhslee0120@gmail.com>
2025-12-10 09:50:20 +0900

    위에서 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력하기

--- src/calculator/App.java
+++ src/calculator/App.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
@@ -8,14 +9,62 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 양의 정수(0을 포함)를 입력하세요: ");
-        int num1 = sc.nextInt();
+        try {
+            System.out.print("첫 번째 양의 정수(0을 포함)를 입력하세요: ");
+            int num1 = sc.nextInt();
 
-        System.out.print("사칙연산 기호를 입력하세요(+, -, *, /): ");
-        char op = sc.next().charAt(0);
+            if (num1 < 0) {
+                System.out.println("양의 정수(0 포함)를 입력해야합니다."); // 양의 정수(0 포함)를 입력하지 않은 경우 종료
+                return;
+            }
 
-        System.out.print("두 번째 양의 정수(0을 포함)를 입력하세요: ");
-        int num2 = sc.nextInt();
+            System.out.print("사칙연산 기호를 입력하세요(+, -, *, /): ");
+            char op = sc.next().charAt(0);
+
+            System.out.print("두 번째 양의 정수(0을 포함)를 입력하세요: ");
+            int num2 = sc.nextInt();
+
+            if (num2 < 0) {
+                System.out.println("양의 정수(0 포함)를 입력해야합니다."); // 양의 정수(0 포함)를 입력하지 않은 경우 종료
+                return;
+            }
+
+            int result = 0;
+
+            switch (op) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+
+                case '-':
+                    result = num1 - num2;
+                    break;
+
+                case '*':
+                    result = num1 * num2;
+                    break;
+
+                case '/':
+                    // 0으로 나눌수 없도록
+                    if (num2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 숫자)에 0이 입력될 수 없습니다.");
+                        return;
+                    } else {
+                        result = num1 / num2;
+                        break;
+                    }
+
+                default:
+                    System.out.println("지원하지 않는 사칙연산 기호입니다."); // 잘못된 사칙연산 기호 입력시 종료
+                    return;
+
+            }
+            System.out.println("결과: " + result);
+
+        } catch (InputMismatchException e) {
+            System.out.println("양의 정수(0 포함)를 입력해주세요."); // 첫번째 숫자, 두번째 숫자에서 정수를 제외한 나머지를 입력한 경우
+
+        }
 
     }
 }
